a86485f7b5f5ecf8a9aa7665f279e215b1a9467e
bakarr86 <bakarr19861@gmail.com>
2024-08-31 09:48:46 -0400

    searchBreed and getBreed detail functions

--- js/api.js
+++ js/api.js
@@ -14,3 +14,29 @@ export async function getBreeds() {
         throw error;
     }
 }
+//search breed function
+export async function searchBreeds(query) {
+    try {
+        const response = await fetch(`${DOG_API_BASE_URL}/breeds/search?q=${query}`);
+        if (!response.ok) {
+            throw new Error('Failed to search breeds');
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error searching breeds:', error);
+        throw error;
+    }
+}
+//getBreed detail
+export async function getBreedDetails(breedId) {
+    try {
+        const response = await fetch(`${DOG_API_BASE_URL}/breeds/${breedId}`);
+        if (!response.ok) {
+            throw new Error('Failed to fetch breed details');
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching breed details:', error);
+        throw error;
+    }
+}
